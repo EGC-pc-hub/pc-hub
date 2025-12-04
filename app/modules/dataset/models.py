5c7879111939f2f507e2cbc0b592b56fdb775f04
alevelmol <alevelmol@alum.us.es>
2025-12-04 17:06:03 +0100

    fix(dataset): flake8 error fixed

--- app/modules/dataset/models.py
+++ app/modules/dataset/models.py
@@ -13,6 +13,7 @@ class PublicationType(Enum):
     HARDWARE = "HARDWARE"
     OTHER = "OTHER"
 
+
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
