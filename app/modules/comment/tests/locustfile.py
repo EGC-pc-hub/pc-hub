7a2c869877ed06d54c776a5ee6e16c63f3c0e7df
Petercrgz <petcargon@alum.us.es>
2025-12-07 19:24:04 +0100

    test(comment): load test for comment module upgraded

--- app/modules/comment/tests/locustfile.py
+++ app/modules/comment/tests/locustfile.py
@@ -4,28 +4,30 @@ from locust import HttpUser, TaskSet, task
 
 from core.environment.host import get_host_for_locust_testing
 
+PARENT_ID = 1
 DATASET_ID = 1
 
 
 class CommentBehavior(TaskSet):
 
-    # Get comments for a dataset
     @task(1)
-    def get_comments_by_dataset(self):
-        url = f"/comment/dataset/{DATASET_ID}"
-        response = self.client.get(url)
-        if response.status_code != 200:
-            print(f"GET {url} failed: {response.status_code}")
+    def get_replies_by_dataset(self):
+        # catch_response is used to make locust able to mark requests as success despite being 4xx
+        with self.client.get(f"/comment/parent/{PARENT_ID}", name="comment:by_dataset", catch_response=True) as resp:
+            # Acceptable responses are 200 (OK), 403 (hidden parent comment), and 404 (parent comment not found)
+            if resp.status_code in (200, 403, 404):
+                resp.success()
+            else:
+                resp.failure(f"Unexpected status: {resp.status_code}")
 
-    # Post a comment to a dataset
     @task(2)
     def post_comment_to_dataset(self):
         url = f"/comment/dataset/{DATASET_ID}/create"
         content = "".join(random.choices("abcdefghijklmnopqrstuvwxyz ", k=50))
 
         response = self.client.post(url, json={"content": content})
-        if response.status_code != 201:
-            print(f"POST {url} failed: {response.status_code}, {response.text}")
+        # 302 expected due to not being logged in
+        assert response.status_code in (302)
 
 
 class CommentUser(HttpUser):
